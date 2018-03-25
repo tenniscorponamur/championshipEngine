ec7a3813d056814fad052b7ebaf04781aaa7b273
fabricecalay <fabrice.calay@gmail.com>
2018-03-25 15:35:18 +0200

    preparation authentification

--- src/main/java/be/company/fca/controller/UserController.java
+++ src/main/java/be/company/fca/controller/UserController.java
@@ -1,11 +1,13 @@
 package be.company.fca.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     @RequestMapping("/user")
