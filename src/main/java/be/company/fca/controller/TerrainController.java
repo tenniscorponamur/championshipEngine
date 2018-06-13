216ebc467c69f6a2ebcce3ea0177d48424cbd44e
Fabrice Calay <fabrice.calay@gmail.com>
2018-06-13 08:31:22 +0200

    gestion des terrains

---
+++ src/main/java/be/company/fca/controller/TerrainController.java
@@ -0,0 +1,43 @@
+package be.company.fca.controller;
+
+import be.company.fca.model.Club;
+import be.company.fca.model.Terrain;
+import be.company.fca.repository.ClubRepository;
+import be.company.fca.repository.TerrainRepository;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+//@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api/v1")
+@Api(description = "API REST pour la gestion des terrains")
+public class TerrainController {
+
+    @Autowired
+    private TerrainRepository terrainRepository;
+
+    @RequestMapping(path="/public/terrains", method= RequestMethod.GET)
+    Iterable<Terrain> getAllTerrains() {
+        return terrainRepository.findAll();
+    }
+
+    @RequestMapping(path="/public/terrain", method= RequestMethod.GET)
+    Terrain getTerrain(@RequestParam Long id) {
+        return terrainRepository.findOne(id);
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN_USER')")
+    @RequestMapping(value = "/private/terrain", method = RequestMethod.PUT)
+    public Terrain updateTerrain(@RequestBody Terrain terrain){
+        return terrainRepository.save(terrain);
+    }
+
+    @PreAuthorize("hasAuthority('ADMIN_USER')")
+    @RequestMapping(value = "/private/terrain", method = RequestMethod.POST)
+    public Terrain addTerrain(@RequestBody Terrain terrain){
+        return terrainRepository.save(terrain);
+    }
+
+}
---
+++ src/main/java/be/company/fca/repository/TerrainRepository.java
@@ -0,0 +1,7 @@
+package be.company.fca.repository;
+
+import be.company.fca.model.Terrain;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TerrainRepository extends CrudRepository<Terrain, Long> {
+}
