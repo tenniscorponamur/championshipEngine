af4ef4f29c42101334394a7479f6c159294051ce
fabricecalay <fabrice.calay@gmail.com>
2018-07-17 18:20:17 +0200

    test api aftnet.be

--- src/main/java/be/company/fca/controller/ClassementAFTController.java
+++ src/main/java/be/company/fca/controller/ClassementAFTController.java
@@ -4,8 +4,13 @@ import be.company.fca.model.ClassementAFT;
 import be.company.fca.repository.ClassementAFTRepository;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -26,4 +31,35 @@ public class ClassementAFTController {
         return classementAFTRepository.save(classementAFT);
     }
 
+
+    @RequestMapping(value = "/testAFT/{numAft}", method = RequestMethod.GET)
+    public String testAFT(@PathVariable("numAft") String numAft){
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        //String requestJson = "{text:\"6065450\"}";
+        String requestJson = "{text:" + numAft + "}";
+
+        HttpEntity<String> request = new HttpEntity<>(requestJson,headers);
+        String result = restTemplate.postForObject("http://www.aftnet.be/MyAFT/Players/GetPlayersAutocomplete", request, String.class);
+
+        return result;
+    }
+
+
+    public static void main(String[] args){
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        String requestJson = "{text:\"6065450\"}";
+
+        HttpEntity<String> request = new HttpEntity<>(requestJson,headers);
+        String result = restTemplate.postForObject("http://www.aftnet.be/MyAFT/Players/GetPlayersAutocomplete", request, String.class);
+
+        System.err.println(result);
+
+    }
 }
