36f8d9032a6e6d95a4ffb8abb09666900a489f0f
fabricecalay <fabrice.calay@gmail.com>
2018-09-12 18:42:28 +0200

    ajout des categories pour le criterium

--- src/main/java/be/company/fca/model/CategorieChampionnat.java
+++ src/main/java/be/company/fca/model/CategorieChampionnat.java
@@ -4,6 +4,10 @@ public enum CategorieChampionnat {
 
     MESSIEURS ("Messieurs"),
     DAMES ("Dames"),
+    SIMPLE_MESSIEURS ("Simples Messieurs"),
+    DOUBLE_MESSIEURS ("Doubles Messieurs"),
+    SIMPLE_DAMES ("Simples Dames"),
+    DOUBLE_DAMES ("Doubles Dames"),
     MIXTES ("Mixtes");
 
     private String libelle = "";
