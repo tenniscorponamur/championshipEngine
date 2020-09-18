a2d2993b3aba340e2e36653359b951e8a777fbcb
fca <fabrice.calay@gmail.com>
2020-09-18 15:38:57 +0200

    ajout d'informations complementaires pour les terrains

--- src/main/java/be/company/fca/model/Terrain.java
+++ src/main/java/be/company/fca/model/Terrain.java
@@ -30,6 +30,18 @@ public class Terrain {
     @Column( name =  "adresse")
     private String adresse;
 
+    @Column( name =  "nom_contact")
+    private String nomContact;
+
+    @Column( name =  "mail")
+    private String mail;
+
+    @Column( name =  "telephone")
+    private String telephone;
+
+    @Column( name =  "infos_restauration")
+    private String infosRestauration;
+
     @Column( name = "show_drink_details", nullable = false)
     private boolean showDrinkDetails=false;
 
@@ -78,6 +90,38 @@ public class Terrain {
 
     public void setAdresse(String adresse) {
         this.adresse = adresse;
+    }
+
+    public String getNomContact() {
+        return nomContact;
+    }
+
+    public void setNomContact(String nomContact) {
+        this.nomContact = nomContact;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getInfosRestauration() {
+        return infosRestauration;
+    }
+
+    public void setInfosRestauration(String infosRestauration) {
+        this.infosRestauration = infosRestauration;
     }
 
     public boolean isShowDrinkDetails() {
---
+++ src/main/resources/db/migration/V1.14.2__infosSupplTerrains.sql
@@ -0,0 +1,5 @@
+alter table terrain add column infos_restauration varchar(2000);
+alter table terrain add column nom_contact varchar(255);
+alter table terrain add column mail varchar(255);
+alter table terrain add column telephone varchar(255);
+
